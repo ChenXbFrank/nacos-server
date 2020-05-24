fcf93bf68ba615319405c4dd336e7305aa5d9ca8
ChenXbFrank <810464826@qq.com>
2020-05-24 09:23:09 +0800

    first commit

---
+++ src/main/java/com/cxb/springcloud/web/ServerController.java
@@ -0,0 +1,18 @@
+package com.cxb.springcloud.web;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ServerController {
+
+    @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+    public String echo(@PathVariable String string) {
+        return "hello Nacos Discovery " + string;
+    }
+
+    @RequestMapping(value = "/divide", method = RequestMethod.GET)
+    public String divide(@RequestParam Integer a, @RequestParam Integer b) {
+        return String.valueOf(a / b);
+    }
+}
